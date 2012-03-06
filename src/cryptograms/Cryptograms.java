4fb5b59223d64f4d36d176aac02c9aee41836117
Charles Caldwell <bookedge@gmail.com>
2012-03-06 06:52:40 -0600

    Removed Array tests I wrote in there yesterday

--- src/cryptograms/Cryptograms.java
+++ src/cryptograms/Cryptograms.java
@@ -1,4 +1,3 @@
-
 package cryptograms;
 
 import java.util.Arrays;
@@ -13,17 +12,6 @@ public class Cryptograms {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Testing Arrays");
-        int[] five = new int[5];
         
-        
-        
-        int[] one = {1,2,3,4,5};
-        int[] two = {1,2,3,4,5};
-        int[] six = {1,2,3,2,4,2,5};
-        if (Arrays.equals(one, two))
-            System.out.println("one and two are equal");
-        else
-            System.out.println("one and two aren't equal");
     }
 }
