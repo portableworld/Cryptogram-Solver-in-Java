26effc88f17e75165791e1bb6b44f750ddbd2820
Charles Caldwell <bookedge@gmail.com>
2012-03-06 09:04:44 -0600

    Organized Imports to contain no wildcards.

--- src/cryptograms/Words.java
+++ src/cryptograms/Words.java
@@ -1,7 +1,9 @@
-
 package cryptograms;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
