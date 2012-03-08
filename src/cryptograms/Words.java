ffdf124ad020d2a8bb8203fdef5c6c1d6200b83f
Charles Caldwell <bookedge@gmail.com>
2012-03-08 09:27:34 -0600

    Made class variable +allWords+ private and added comments for Javadoc.

--- src/cryptograms/Words.java
+++ src/cryptograms/Words.java
@@ -12,13 +12,22 @@ import java.util.ArrayList;
  */
 public class Words {
     
-    ArrayList<String> allWords = new ArrayList<String>();
+    private ArrayList<String> allWords = new ArrayList<String>();
     
+    /**
+     * Class constructor
+     * <p>
+     * Reads a built in text file as its dictionary of words.
+     * 
+     */ 
     public Words() {
         try {
             String word;
+            // Get text file of Dictionary words. 
+            // TODO - Maybe make into a loop for adding multiple Dictionaries?
             InputStream file = getClass().getResourceAsStream("2of4brif.txt");
             BufferedReader reader = new BufferedReader(new InputStreamReader(file));
+            // In Dictionary file, each line is a single word. 
             while ((word = reader.readLine()) != null) {
                 allWords.add(word);
             }
@@ -27,6 +36,13 @@ public class Words {
         }
     }
     
+    /**
+     * Gets all the words from the class's dictionary that have an equal 
+     * length as the int passed in.
+     * 
+     * @param   len   Length of the words to pull out of the Dictionary
+     * @return        A String Array of all the words in the Dictionary of the given length
+     */
     public String[] getWordsOfLength(int len) {
         ArrayList<String> foundWords = new ArrayList<String>();
         for (String word : allWords) {
